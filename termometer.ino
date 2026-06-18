00d1537ef747609f7588b75a31e1cdfe3a3190c1
Kaczi <anna.pokoraaa@interia.pl>
2026-06-18 15:11:18 +0200

    add temperature averaging and collect samples into buffer

--- termometer.ino
+++ termometer.ino
@@ -18,9 +18,13 @@ DS18B20 ds(4);
 uint8_t address[] = {0x28, 0x0E, 0x6E, 0x22, 0x00, 0x00, 0x00, 0xC9};
 uint8_t selected;
 
-void setup() {
-  Serial.begin(9600);
-  selected = ds.select(address);
+float temperature;
+const uint16_t BUFFER_SIZE = 20;
+float tempBuffer[BUFFER_SIZE];
+bool isBufferFull = false;
+uint16_t sample = 0;
+
+void displayInit(){
   tft.initR(INITR_BLACKTAB); 
   tft.fillScreen(ST77XX_WHITE);
   tft.setRotation(1);
@@ -28,16 +32,53 @@ void setup() {
   tft.setTextSize(2);
   tft.setTextColor(ST77XX_BLACK);
   tft.println("Temperature:");
+  
+}
+
+float getMeanTemperature(){
+  float temperature = 0;
+  float tabTemperature[5];
+    for(int i = 0; i < 5; i++){
+      tabTemperature[i] = ds.getTempC();
+      Serial.println(tabTemperature[i]);
+      temperature += tabTemperature[i];
+      delay(100);
+    }
+    temperature = temperature/5;
+    Serial.println("Temperatura uśredniona: " + String(temperature));
+    return temperature;
+}
+
+void setup() {
+  Serial.begin(9600);
+  selected = ds.select(address);
+  displayInit();
 }
 
 void loop() {
   if (selected) {
-    float temperature = ds.getTempC();
-    Serial.println(temperature);
+    //zebranie uśrednionej temperatury z 5 pomiarów co sekundę
+    temperature = getMeanTemperature();
     tft.setCursor(40, 50);
-    tft.println(temperature);
+    tft.fillRect(40, 50, 160, 25, ST77XX_WHITE);
+
+    if(sample < BUFFER_SIZE){
+      tempBuffer[sample++] = temperature;
+    }
+    else{
+      isBufferFull = true;
+    }
+
+    if(isBufferFull){
+      for(int i = 0; i < 10; i++){
+        Serial.println("Temperatura z bufora: ");
+        Serial.println(tempBuffer[i]);
+        delay(100);
+      }
+      sample = 0;
+      isBufferFull = false;
+    }
+
   }
-  delay(5000);
-  tft.fillRect(40, 50, 160, 25, ST77XX_WHITE);
-  
+  delay(100);
 }
