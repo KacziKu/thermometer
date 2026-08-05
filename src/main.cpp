350e7c158d8edbe8218cfe3a4e8b65e7e0477e5e
Kaczi <anna.pokoraaa@interia.pl>
2026-08-05 16:55:27 +0200

    feat: add flash task, encoder task and display task.
    First simple version

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ struct tm timeinfo;
 uint8_t view = 0;
 uint8_t markerPosition;
 float temperature = 28;
+int16_t move = 0;
 
 QueueHandle_t recordQueue;
 
@@ -37,6 +38,57 @@ void sampleMeasure(void *pvParameters) {
     
     xQueueSend(recordQueue, &record, portMAX_DELAY);
     xTaskDelayUntil(&lastWakeTime, 3000000);
+  }
+}
+
+void flashWrite(void *pvParameters) {
+  while(1) {
+    Record record;
+    xQueueReceive(recordQueue, &record, portMAX_DELAY);
+
+    int address = getCurrentAddress();
+    int sectorAddress = getNextSectorToErase(address);
+    clearSector(sectorAddress);
+    writeRecord(address, &record);
+  }
+}
+
+void encoderTask(void *pvParameters) {
+  while(1) {
+    if(encoder.readButton()) {
+      view = (view + 1) % 3;
+      tft.fillScreen(ST7735_WHITE);
+    }
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
+void displayTask(void *pvParameters) {
+  while(1) {
+    switch(view) {
+    case MAIN:
+      drawIcons();
+      writeTemperature(temperature, 40, 95, 100, 25, 2);
+      writeData(timeinfo, 40, 55, 100, 16, 2);
+      writeTime(timeinfo, 40, 15, 100, 16, 2);
+      break;
+    case PLOT:
+      drawAxis();
+      drawPlot(&buff);
+      move = encoder.readRotation();
+      markerPosition = drawMarker(move);
+      //printf("Marker position: %d\n", markerPosition);
+      writeTemperature(buff.buffer[markerPosition], 50, 5, 80, 15, 1);
+      writeTime(timeinfo, 105, 5, 80, 15, 1);
+      break;
+    case HISTORY:
+      drawHistoryTemplate();
+      writeData(timeinfo, 35, 8, 98, 16, 2);
+      writeTemperature(temperature, 75, 58, 55, 15, 1);
+      writeTemperature(temperature, 75, 83, 55, 15, 1);
+      break;
+    }
+    vTaskDelay(pdMS_TO_TICKS(30));
   }
 }
 
@@ -65,41 +117,13 @@ void setup() {
   displayInit();
 
   recordQueue = xQueueCreate(10, sizeof(Record));
-  xTaskCreate(sampleMeasure, "Sample Measure", 1024, NULL, 0, NULL);
+  xTaskCreate(sampleMeasure, "Sample Measure", 4096, NULL, 0, NULL);
+  xTaskCreate(flashWrite, "Flash write", 4096, NULL, 0, NULL);
+  xTaskCreate(encoderTask, "Encoder Task", 1024, NULL, 0, NULL);
+  xTaskCreate(displayTask, "Display Task", 4096, NULL, 0, NULL);
 }
 
 
 void loop() {
-  
-  if(encoder.readButton()) {
-    view = (view + 1) % 3;
-    tft.fillScreen(ST7735_WHITE);
-  }
 
-  int16_t move = encoder.readRotation();
-
-  switch(view) {
-    case MAIN:
-      drawIcons();
-      writeTemperature(temperature, 40, 95, 100, 25, 2);
-      writeData(timeinfo, 40, 55, 100, 16, 2);
-      writeTime(timeinfo, 40, 15, 100, 16, 2);
-     
-      break;
-    case PLOT:
-      drawAxis();
-      drawPlot(&buff);
-      markerPosition = drawMarker(move);
-      printf("Marker position: %d\n", markerPosition);
-      writeTemperature(buff.buffer[markerPosition], 50, 5, 80, 15, 1);
-      writeTime(timeinfo, 105, 5, 80, 15, 1);
-      
-      break;
-    case HISTORY:
-      drawHistoryTemplate();
-      writeData(timeinfo, 35, 8, 98, 16, 2);
-      writeTemperature(temperature, 75, 58, 55, 15, 1);
-      writeTemperature(temperature, 75, 83, 55, 15, 1);
-      break;
-  }
 }
